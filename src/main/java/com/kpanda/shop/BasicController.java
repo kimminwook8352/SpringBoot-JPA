ccfec70f701b9c87d38925ae4b63ee6cc653f1e5
Kpanda <minuk8352@gmail.com>
2024-05-28 17:10:00 +0900

    JPA CRUD 연습1

--- src/main/java/com/kpanda/shop/BasicController.java
+++ src/main/java/com/kpanda/shop/BasicController.java
@@ -1,19 +1,27 @@
 package com.kpanda.shop;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class BasicController {
+
+    private final ItemRepository itemRepository;
+
     @GetMapping("/")
-    @ResponseBody
-    String test(){
-        return "list.html";
+    String index(Model model){
+        List<Item> result = itemRepository.findAll();
+        model.addAttribute("items", result);
+        return "list";
     }
 
     @GetMapping("/about")
